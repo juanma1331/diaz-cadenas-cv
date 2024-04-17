97e84be17e5f574182f6c88d2f97b98ad51376c8
juanma1331 <juanma1331@outlook.es>
2024-04-17 11:28:53 +0200

    Update dependencies and add Cloudflare Workers support for uploadthing API

--- src/pages/api/uploadthing.ts
+++ src/pages/api/uploadthing.ts
@@ -1,10 +1,56 @@
 import { createRouteHandler } from "uploadthing/server";
 
-import { uploadRouter } from "../../server/uploadthing";
+import { uploadRouter } from "@/server/uploadthing";
+import type { ExecutionContext } from "@cloudflare/workers-types";
 
-export const { GET, POST } = createRouteHandler({
-  router: uploadRouter,
-  config: {
-    uploadthingSecret: import.meta.env.UPLOADTHING_SECRET,
+export interface Env {
+  UPLOADTHING_SECRET: string;
+  UPLOADTHING_APP_ID: string;
+  ENVIRONMENT: string;
+}
+
+export default {
+  async fetch(request: Request, env: Env, ctx: ExecutionContext) {
+    const handlers = createRouteHandler({
+      router: uploadRouter,
+      config: {
+        /**
+         * Since workers doesn't have envs on `process`. We need to pass
+         * secret and isDev flag manually.
+         */
+        uploadthingSecret: env.UPLOADTHING_SECRET,
+        isDev: env.ENVIRONMENT === "development",
+        /*
+         * Cloudflare Workers doesn't support the cache option
+         * so we need to remove it from the request init.
+         */
+        fetch: (url, init) => {
+          if (init && "cache" in init) delete init.cache;
+          return fetch(url, init);
+        },
+      },
+    });
+
+    // World's simplest router. Handle GET/POST requests to /api/uploadthing
+    switch (new URL(request.url).pathname) {
+      case "/api/uploadthing": {
+        if (request.method !== "POST" && request.method !== "GET") {
+          return new Response("Method not allowed", { status: 405 });
+        }
+
+        const response = await handlers[request.method](request);
+        if ("cleanup" in response && response.cleanup) {
+          /**
+           * UploadThing dev server leaves some promises hanging around that we
+           * need to wait for to prevent the worker from exiting prematurely.
+           */
+          ctx.waitUntil(response.cleanup);
+        }
+        return response;
+      }
+      default: {
+        return new Response("Not found", { status: 404 });
+      }
+    }
   },
-});
+};
