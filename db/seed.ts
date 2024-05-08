df0355cb195e111ff83a5c05b69ba785ca54878b
juanma1331 <juanma1331@outlook.es>
2024-05-08 13:00:42 +0200

    Refactor generateCVS function to accept an array of operations

--- db/seed.ts
+++ db/seed.ts
@@ -4,72 +4,75 @@ import { faker } from "@faker-js/faker";
 
 // https://astro.build/db/seed
 export default async function seed() {
-  await generateCVS(1000);
+  await generateCVs([{ qty: 1000 }]);
 }
 
-async function generateCVS(qty: number) {
+function defaultCV(id: string) {
+  return {
+    id,
+    name: faker.person.fullName(),
+    email: faker.internet.email(),
+    place: faker.helpers.arrayElement([
+      "Andújar",
+      "Brenes",
+      "Bollullos Par del Condado",
+      "Cádiz",
+      "Coria del Rio",
+      "Estepa",
+      "Gilena",
+      "Hytasa",
+      "La Carolina",
+      "Lantejuela",
+      "Moguer",
+      "Osuna",
+      "Sanlúcar de Barrameda",
+      "Sevilla",
+      "Utrera",
+    ]),
+    position: faker.helpers.arrayElement([
+      "Carnicería",
+      "Charcutería",
+      "Pescadería",
+      "Frutería",
+      "Panadería",
+      "Pastelería",
+      "Cajero",
+      "Reponedor",
+      "Limpieza",
+    ]),
+    status: faker.helpers.arrayElement([1, 2, 3, 4]),
+    createdAt: faker.date.past(),
+  };
+}
+
+type Operation = {
+  qty: number;
+  attributes?: Partial<typeof CVS.$inferInsert>;
+};
+
+async function generateCVs(operations: Array<Operation>) {
   const queries = [];
-  for (let i = 0; i < qty; i++) {
-    const cvId = generateId(15);
-    queries.push(
-      db.insert(CVS).values({
-        id: cvId,
-        name: faker.person.fullName(),
-        email: faker.internet.email(),
-        place: faker.helpers.arrayElement([
-          "Andújar",
-          "Brenes",
-          "Bollullos Par del Condado",
-          "Cádiz",
-          "Coria del Rio",
-          "Estepa",
-          "Gilena",
-          "Hytasa",
-          "La Carolina",
-          "Lantejuela",
-          "Moguer",
-          "Osuna",
-          "Sanlúcar de Barrameda",
-          "Sevilla",
-          "Utrera",
-        ]),
-        position: faker.helpers.arrayElement([
-          "Carnicería",
-          "Charcutería",
-          "Pescadería",
-          "Frutería",
-          "Panadería",
-          "Pastelería",
-          "Cajero",
-          "Reponedor",
-          "Limpieza",
-        ]),
-        status: faker.helpers.arrayElement([1, 2, 3, 4]),
-        createdAt: faker.date.past(),
-      })
-    );
+  for (const operation of operations) {
+    for (let i = 0; i < operation.qty; i++) {
+      const cvId = generateId(15);
+      const CV = {
+        ...defaultCV(cvId),
+        ...operation.attributes,
+      } as typeof CVS.$inferInsert;
 
-    queries.push(
-      db.insert(ATTACHMENTS).values({
-        name: `Attachment ${i}`,
-        url: `https://example.com/attachment-${i}`,
-        size: 4194304,
-        type: "application/pdf",
-        key: `key-${i}`,
-        cvId: cvId,
-      })
-    );
+      queries.push(db.insert(CVS).values(CV));
 
-    queries.push(
-      db.insert(ATTACHMENTS).values({
-        name: `Attachment-2 ${i}`,
-        url: `https://example.com/attachment-2-${i}`,
-        size: 35651584,
-        type: "video/mp4",
-        key: `key-2-${i}`,
-        cvId: cvId,
-      })
-    );
+      queries.push(
+        db.insert(ATTACHMENTS).values({
+          name: `Attachment ${i}`,
+          url: `https://example.com/attachment-${i}`,
+          size: 4194304,
+          type: "application/pdf",
+          key: `key-${i}`,
+          cvId: cvId,
+        })
+      );
+    }
   }
 
   await db.batch(queries as any);
